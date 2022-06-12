0f7b14b8a54ed3f7992f767f95b0b8e13407ca98
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2022-06-12 17:18:35 -0300

    #Inclusão do método update de JSON.

--- integracoes/cliente-importar.php
+++ integracoes/cliente-importar.php
@@ -6,17 +6,38 @@
 	$array = json_decode($json, true);    
 	
 	foreach($array as $info) {
-		$sql = "INSERT INTO pessoa(id,nome,tipo,documento,endereco,telefone,email,status,cliente) 
-                    VALUES(null, '{$info['nome']}', lower('{$info['tipo']}'), '{$info['documento']}',
-                            '{$info['endereco']}','{$info['telefone']}','{$info['email']}',upper('{$info['status']}'),
-                            upper('{$info['cliente']}'))";
+        if(count($info) == 9){
+            $sqlPessoa = "SELECT * FROM pessoa WHERE id = " . $info['id'];
+            $queryPessoa = mysqli_query($conexao, $sqlPessoa);
+            if(!$queryPessoa) {
+                echo mysqli_error($conexao);
+            } else {
+                $item = mysqli_fetch_array($queryPessoa, MYSQLI_ASSOC);
+    
+                if($info['id'] == $item['id']){
+                    $sql = "UPDATE pessoa
+                            SET id={$info['id']}, nome='{$info['nome']}', tipo=lower('{$info['tipo']}'), documento='{$info['documento']}',
+                                endereco='{$info['endereco']}', telefone='{$info['telefone']}', email='{$info['email']}', status=upper('{$info['status']}'), cliente=upper('{$info['cliente']}')
+                            WHERE id={$info['id']}"; 
+                            echo $sql;          
+                } else {
+                    $mensagemErro = "Não foi possível alterar o cliente! Erro: " . mysqli_error($conexao) . ". ";
+                }
+            }
+        } else {
+            $sql = "INSERT INTO pessoa(id,nome,tipo,documento,endereco,telefone,email,status,cliente) 
+            VALUES(null, '{$info['nome']}', lower('{$info['tipo']}'), '{$info['documento']}',
+                    '{$info['endereco']}','{$info['telefone']}','{$info['email']}',upper('{$info['status']}'),
+                    upper('{$info['cliente']}'))";
+        }
 
         $query = mysqli_query($conexao,$sql);
         if($query){
             header('Location: ../views/cliente-listar.php?ok=1');
         } else {
-            $_SESSION['importacao-cliente'] = $json;
-            $mensagemErro = "Não foi possível cadastrar a pessoa! Erro: " . mysqli_error($conexao) . ". ";
+            $_SESSION['importacao-cliente'] = $json;            
+            $mensagemErro = "Não foi possível cadastrar a cliente! Erro: " . mysqli_error($conexao) . ". ";
+            echo $mensagemErro;
             header('Location: ../views/integracao-cliente-importar.php?msg=' . $mensagemErro);
         }
 	}
