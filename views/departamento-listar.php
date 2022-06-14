78422406b1f9c6313644efc79e8d22e40270629d
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2022-06-14 20:44:16 -0300

    #modelo de exclusão via ajax

--- views/departamento-listar.php
+++ views/departamento-listar.php
@@ -50,15 +50,15 @@
                         <td><?php echo $item['id']?></td>
                         <td><?php echo $item['nome']?></td>
                         <td><a href="departamento-editar.php?id=<?php echo $item['id']?>"><img src="../assets/editar.png" alt="Editar" title="Editar"></a></td>
-                        <td><a href="../controllers/departamento/departamento-excluir.php?id=<?php echo $item['id']?>"><img src="../assets/excluir.png" alt="Excluir" title="Excluir" id="btnExcluir"></a></td>
-                    </tr>			
+                        <td id="<?php echo $item['id']?>"><input type="hidden" name="idExcluir" id="idExcluir" value=""><img src="../assets/excluir.png" alt="Excluir" title="Excluir" id="btnExcluir"></td>                        
+                    </tr>
                     <?php
                             }
                         }
-                    ?>          
-                </tbody>  
+                    ?>
+                </tbody>
             </table>
-        </section>			
+        </section>
 	</body>
 </html>
 <?php
@@ -68,7 +68,18 @@
 <script type="text/javascript">
 	$(document).ready(function () {
 		$('img#btnExcluir').on('click', function () {
-			var excluir = confirm("Deseja excluir o cadastro?");
+            var idExclusao = $(this).closest('td').attr("id");
+            if(confirm("Deseja excluir o cadastro?")){
+                    $.ajax({
+                        url: '../controllers/departamento/departamento-excluir.php?id=' + idExclusao,
+                        method: 'GET',
+                    }).done(function(retorno){
+                        $(this).closest('tr').remove();
+                    }); // ver porque essa parte está quebrando
+                    //$(this).closest('tr').remove();
+            } else {
+                alert('Processo cancelado.');
+            }
 		});
 	});
 </script>
